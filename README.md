b8f90e470f4381480e59b6759684dd3b0be9b269
Mohammad Shadab Hussain <shadab.iiitnr20@gmail.com>
2024-03-27 03:38:49 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Implementation of Micro Front Ends through a demo application
+
---
+++ container/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+  devServer: {
+    port: 8000,
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
