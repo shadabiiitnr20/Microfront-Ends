4b9342a5b7e86d2f85672e947f6e3a210c9cb3cb
Mohammad Shadab Hussain <shadab.iiitnr20@gmail.com>
2024-04-09 20:11:18 +0530

    Added webpack configs to Container App and added tailwindcss

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,24 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+};
